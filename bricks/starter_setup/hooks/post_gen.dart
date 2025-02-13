bf75e030b3112a73113ff1b4ee4a619b9bb12a33
Oluwaseun Oyewale <oyewalekehinde34@gmail.com>
2025-02-13 00:16:13 +0000

    update to bricks

--- bricks/starter_setup/hooks/post_gen.dart
+++ bricks/starter_setup/hooks/post_gen.dart
@@ -2,7 +2,8 @@ import 'package:mason/mason.dart';
 import 'package:process_run/shell.dart';
 
 void run(HookContext context) async {
-  context.logger.info('installed brick for {{name}}!');
+  var name = context.vars['name'];
+  context.logger.info('installed brick for $name!');
   installI10n(context);
 }
 
@@ -10,6 +11,13 @@ Future<void> installI10n(HookContext context) async {
   var shell = Shell();
 
   //await shell.run('flutter pub run intl_utils:generate');
+
+  await shell.run('''
+
+# Display some text
+dart run intl_utils:generate
+
+''');
   context.logger.info(
       'Run this prompt after installiing the brick: dart run intl_utils:generate');
 }
